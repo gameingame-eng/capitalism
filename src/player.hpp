a6a791781eda2b38d139d086549af943f6469258
AndrewGalvez <dogisamoose.amazon@gmail.com>
2026-04-19 16:19:26 -0700

    start attack

--- src/player.hpp
+++ src/player.hpp
@@ -22,6 +22,11 @@ public:
   Vector2 vel = {0, 0};
   float friction = 200;
 
+  int attackSize = 80;
+  Rectangle attackRect = {0, 0, 1, 1};
+  int attackSustain = 0;
+  const int attackSustainMax = 30;
+
   void update(MapTile map[100][100], float dt, Camera2D &gamecam,
               GameState &state, std::vector<Enemy> &enemies) {
     this->hitbox = {this->rect.x + ((this->rect.width / 32.0f) * 10),
@@ -39,6 +44,8 @@ public:
       vel.y -= vel.y >= 0 ? friction : -friction;
     else
       vel.y = 0;
+
+    attack(enemies);
 
     if (IsKeyDown(KEY_A)) {
       vel.x = -this->speed;
@@ -74,37 +81,41 @@ public:
       }
     }
 
-    const int ntxs[2] = {
-        (int)floor((int)(hitbox.x + vel.x * dt) / (int)MAP_TILE_SIZE) + 50,
-        (int)floor((int)(hitbox.x + vel.x * dt + hitbox.width) /
-                   (int)MAP_TILE_SIZE) +
-            50};
-    const int ntys[2] = {
-        (int)floor((int)(hitbox.y + vel.y * dt) / (int)MAP_TILE_SIZE) + 50,
-        (int)floor((int)(hitbox.y + vel.y * dt + hitbox.height) /
-                   (int)MAP_TILE_SIZE) +
-            50};
-
-    const int ctxs[2] = {
-        (int)floor((int)(hitbox.x) / (int)MAP_TILE_SIZE) + 50,
-        (int)floor((int)(hitbox.x + hitbox.width) / (int)MAP_TILE_SIZE) + 50};
-    const int ctys[2] = {
-        (int)floor((int)(hitbox.y) / (int)MAP_TILE_SIZE) + 50,
-        (int)floor((int)(hitbox.y + hitbox.height) / (int)MAP_TILE_SIZE) + 50};
+    auto tileIdx = [](float worldPos) {
+      return (int)floor(worldPos / MAP_TILE_SIZE) + 50;
+    };
 
     auto iswall = [&](int x, int y) {
+      if (x < 0 || x >= 100 || y < 0 || y >= 100)
+        return true;
       return map[y][x].type == MAP_TILE_TYPE_WALL;
     };
 
-    if (!(iswall(ntxs[0], ctys[0]) || iswall(ntxs[0], ctys[1]) ||
-          iswall(ntxs[1], ctys[0]) || iswall(ntxs[1], ctys[1]))) {
+    float nextX = hitbox.x + vel.x * dt;
+    int ntxL = tileIdx(nextX);
+    int ntxR = tileIdx(nextX + hitbox.width - 1);
+    int cyT = tileIdx(hitbox.y);
+    int cyB = tileIdx(hitbox.y + hitbox.height - 1);
+
+    if (!(iswall(ntxL, cyT) || iswall(ntxL, cyB) || iswall(ntxR, cyT) ||
+          iswall(ntxR, cyB))) {
       rect.x += vel.x * dt;
-    }
-    if (!(iswall(ctxs[0], ntys[0]) || iswall(ctxs[0], ntys[1]) ||
-          iswall(ctxs[1], ntys[0]) || iswall(ctxs[1], ntys[1]))) {
-      rect.y += vel.y * dt;
+    } else {
+      vel.x = 0;
     }
 
+    float nextY = hitbox.y + vel.y * dt;
+    int cxL = tileIdx(hitbox.x);
+    int cxR = tileIdx(hitbox.x + hitbox.width - 1);
+    int ntyT = tileIdx(nextY);
+    int ntyB = tileIdx(nextY + hitbox.height - 1);
+
+    if (!(iswall(cxL, ntyT) || iswall(cxR, ntyT) || iswall(cxL, ntyB) ||
+          iswall(cxR, ntyB))) {
+      rect.y += vel.y * dt;
+    } else {
+      vel.y = 0;
+    }
     if (std::abs((this->rect.x + this->rect.width / 2) - gamecam.target.x) >
         100) {
       if (this->rect.x > gamecam.target.x)
@@ -131,10 +142,56 @@ public:
     }
   }
 
+  void attack(std::vector<Enemy> &enemies) {
+    if (attackSustain > 0) {
+      attackSustain--;
+      attackRect = {rect.x + rect.width / 2.0f - attackSize / 2.0f,
+                    rect.y + rect.height / 2.0f - attackSize / 2.0f,
+                    (float)attackSize, (float)attackSize};
+
+      if (facing == 0) {
+        attackRect.y += rect.height / 2.0f + attackSize;
+        attackRect.width = attackSize * 2;
+        attackRect.x -= attackSize / 2.0f;
+      }
+      if (facing == 1) {
+        attackRect.x -= rect.width / 2.0f + attackSize;
+        attackRect.height = attackSize * 2;
+        attackRect.y -= attackSize / 2.0f;
+      }
+      if (facing == 2) {
+        attackRect.x += rect.width / 2.0f + attackSize;
+        attackRect.height = attackSize * 2;
+        attackRect.y -= attackSize / 2.0f;
+      }
+      if (facing == 3) {
+        attackRect.y -= rect.height / 2.0f + attackSize;
+        attackRect.width = attackSize * 2;
+        attackRect.x -= attackSize / 2.0f;
+      }
+
+    } else
+      attackRect = {0, 0, 0, 0};
+
+    for (auto &e : enemies) {
+      if (GetCollisionRec(attackRect, e.rect).width != 0) {
+        Vector2 v = Vector2Negate(Vector2Scale(
+            Vector2Normalize(Vector2Subtract(
+                {hitbox.x - hitbox.width / 2, hitbox.y - hitbox.height / 2},
+                {e.rect.x - e.rect.width / 2, e.rect.y - e.rect.height / 2})),
+            1600));
+        e.vel = v;
+      }
+    }
+    if (IsKeyPressed(KEY_SPACE))
+      attackSustain = attackSustainMax;
+  }
+
   void draw() {
     DrawTexturePro(
         characters,
         GetPlayerSpriteSource(this->state, this->anim_frame, this->facing),
         this->rect, {0, 0}, 0.0f, WHITE);
+    DrawRectangleRec(attackRect, BLUE);
   }
 };
